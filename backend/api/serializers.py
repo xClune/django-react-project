8f07dcb7ed00e2ca31c2845588df3de4e5d3fcac
xClune <aclune382@gmail.com>
2024-04-04 12:27:18 +0800

    Fix minor errors, test on server with user POST.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class userSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh")
-    path("api-auth/", include("rest_framework.urls"))
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
 ]
