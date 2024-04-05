b1abcff0a44804b164f13fad3c1a737e86abdfb0
xClune <aclune382@gmail.com>
2024-04-05 13:38:48 +0800

    Function for get, create and delete note in home page using backend urls to retrieve data.
    Add form for creating new note.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,79 @@ import {useState, useEffect} from 'react'
 import api from '../api'
 
 function Home() {
-    const [notes, setNotes] = useState([]);
-    const 
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
 
-    return <div>Home</div>
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    const getNotes = () => {
+        api
+            .get("api/notes/")
+            .then((res) => res.data)
+            .then((data) => { setNotes(data); console.log(data) })
+            .catch((err) => alert(err))
+    }
+
+    const deleteNote = (id) => {
+        api
+            .delete(`api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert('Note deleted successfully!')
+                else alert('Failed to delete note.')
+            })
+            .catch((error) => alert(error))
+        getNotes()
+    }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api
+            .post('/api/notes', {content, title})
+            .then((res) => {
+                if (res.status === 201) alert('Note created successfully!')
+                else alert('Failed to create note.')
+            })
+            .catch((error) => alert(error))
+        getNotes()
+    }
+
+    return (
+        <>
+            <div>
+                <h2>Notes</h2>
+                {notes.map((note) => (
+                    <Note note={note} onDelete={deleteNote} key={note.id} />
+                ))}
+            </div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title:</label>
+                <br />
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <br />
+                <label htmlFor="content">Content:</label>
+                <br />
+                <textarea
+                    id="content"
+                    name="content"
+                    required
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
+        </>);
 }
 
 export default Home
